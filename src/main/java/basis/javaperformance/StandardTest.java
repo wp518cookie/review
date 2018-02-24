f6f7eb3128beda69079cc6c93ec18f8977a7b665
wuping <403704156@qq.com>
2018-02-24 17:34:26 +0800

    20180224

---
+++ src/main/java/basis/javaperformance/StandardTest.java
@@ -0,0 +1,17 @@
+package basis.javaperformance;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2018/2/12.
+ */
+public class StandardTest {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap();
+        String a = "abc";
+        map.put(a, "def");
+        a = "def";
+        System.out.println("abc");
+    }
+}
