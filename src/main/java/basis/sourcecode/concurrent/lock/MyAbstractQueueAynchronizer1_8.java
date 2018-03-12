dcb0c95befea605e97a22dc726cfd7e4b92109ad
wuping <403704156@qq.com>
2018-03-12 17:41:41 +0800

    2018031201

--- src/main/java/basis/sourcecode/concurrent/lock/MyAbstractQueueAynchronizer1_8.java
+++ src/main/java/basis/sourcecode/concurrent/lock/MyAbstractQueueAynchronizer1_8.java
@@ -4,6 +4,7 @@ import sun.misc.Unsafe;
 import util.UnsafeGenerator;
 
 import java.util.concurrent.locks.AbstractOwnableSynchronizer;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * Created by ping.wu on 2018/3/6.
@@ -15,11 +16,322 @@ public class MyAbstractQueueAynchronizer1_8 extends AbstractOwnableSynchronizer 
     private volatile int state;
     //-------------------------------inner Class-----------------------------
     static final class Node {
+        static final Node SHARED = new Node();
+        static final Node EXCLUSIVE = null;
+        static final int CANCELLED = 1;
+        static final int SIGNAL = -1;
+        static final int CONDITION = -2;
+        static final int PROPAGATE = -3;
+        volatile Node next;
+        volatile Thread thread;
+        volatile Node prev;
+        volatile int waitStatus;
+        Node nextWaiter;
 
+        final boolean isShared() {
+            return nextWaiter == SHARED;
+        }
+
+        final Node predecessor() throws NullPointerException {
+            Node p = prev;
+            if (p == null)
+                throw new NullPointerException();
+            else
+                return p;
+        }
+
+        Node() {
+        }
+
+        Node(Thread thread, Node mode) {
+            this.nextWaiter = mode;
+            this.thread = thread;
+        }
+
+        Node(Thread thread, int waitStatus) {
+            this.waitStatus = waitStatus;
+            this.thread = thread;
+        }
     }
     //-------------------------------method---------------------------------
     public final void acquire(int arg) {
+        if (!tryAcquire(arg) && acquireQueued(addWaiter(Node.EXCLUSIVE), arg)) {
+            selfInterrupt();
+        }
+    }
 
+    public final void acquireInterruptibly(int arg) throws InterruptedException {
+        if (Thread.interrupted()) {
+            throw new InterruptedException();
+        }
+        if (!tryAcquire(arg)) {
+            doAcquireInterruptibly(arg);
+        }
+    }
+
+    final boolean acquireQueued(final Node node, int arg) {
+        boolean failed = true;
+        try {
+            boolean interrupted = false;
+            for (; ; ) {
+                final Node p = node.predecessor();
+                if (p == head && tryAcquire(arg)) {
+                    setHead(node);
+                    p.next = null;
+                    failed = false;
+                    return interrupted;
+                }
+                if (shouldParkAfterFailedAcquire(p, node) && parkAndCheckInterrupt()) {
+                    interrupted = true;
+                }
+            }
+        } finally {
+            if (failed) {
+                cancelAcquire(node);
+            }
+        }
+    }
+
+    public final void acquireShared(int arg) {
+        if (tryAcquireShared(arg) < 0) {
+            doAcquireShared(arg);
+        }
+    }
+
+    private Node addWaiter(Node mode) {
+        Node node = new Node(Thread.currentThread(), mode);
+        Node pred = tail;
+        if (pred != null) {
+            node.prev = tail;
+            if (compareAndSetTail(pred, node)) {
+                pred.next = node;
+                return node;
+            }
+        }
+        enq(node);
+        return node;
+    }
+
+    private void cancelAcquire(Node node) {
+        if (node == null) {
+            return;
+        }
+        node.thread = null;
+        Node pred = node.prev;
+        while (pred.waitStatus > 0) {
+            node.prev = pred = pred.prev;
+        }
+        Node predNext = pred.next;
+        node.waitStatus = Node.CANCELLED;
+        if (node == tail && compareAndSetTail(node, pred)) {
+            compareAndSetNext(pred, predNext, null);
+        } else {
+            int ws;
+            if (pred != head && ((ws = pred.waitStatus) == Node.SIGNAL ||
+                    (ws <= 0 && compareAndSetWaitStatus(pred, ws, Node.SIGNAL))) &&
+                    pred.thread != null) {
+                Node next = node.next;
+                if (next != null && next.waitStatus <= 0) {
+                    compareAndSetNext(pred, predNext, next);
+                }
+            } else {
+                unparkSuccessor(node);
+            }
+            node.next = node;
+        }
+    }
+
+    private final boolean compareAndSetHead(Node update) {
+        return unsafe.compareAndSwapObject(this, headOffset, null, update);
+    }
+
+    private final boolean compareAndSetTail(Node expect, Node update) {
+        return unsafe.compareAndSwapObject(this, tailOffset, expect, update);
+    }
+
+    private static final boolean compareAndSetWaitStatus(Node node,
+                                                         int expect,
+                                                         int update) {
+        return unsafe.compareAndSwapInt(node, waitStatusOffset,
+                expect, update);
+    }
+
+    private static final boolean compareAndSetNext(Node node,
+                                                   Node expect,
+                                                   Node update) {
+        return unsafe.compareAndSwapObject(node, nextOffset, expect, update);
+    }
+
+    private void doAcquireInterruptibly(int arg) throws InterruptedException {
+        final Node node = addWaiter(Node.EXCLUSIVE);
+        boolean failed = true;
+        try {
+            for (; ; ) {
+                final Node p = node.predecessor();
+                if (p == head && tryAcquire(arg)) {
+                    setHead(node);
+                    p.next = null;
+                    failed = false;
+                    return;
+                }
+                if (shouldParkAfterFailedAcquire(p, node) && parkAndCheckInterrupt()) {
+                    throw new InterruptedException();
+                }
+            }
+        } finally {
+            if (failed) {
+                cancelAcquire(node);
+            }
+        }
+    }
+
+    private boolean doAcquireNanos(int arg, long nanosTimeout) throws InterruptedException {
+        if (nanosTimeout <= 0L) {
+            return false;
+        }
+        final long deadline = System.nanoTime() + nanosTimeout;
+        final Node node = addWaiter(Node.EXCLUSIVE);
+        boolean failed = true;
+        try {
+            for (; ; ) {
+                final Node p = node.predecessor();
+                if (p == head && tryAcquire(arg)) {
+                    setHead(node);
+                    p.next = null;
+                    failed = false;
+                    return true;
+                }
+                nanosTimeout = deadline - System.nanoTime();
+                if (nanosTimeout <= 0L) {
+                    return false;
+                }
+                if (shouldParkAfterFailedAcquire(p, node) && nanosTimeout > spinForTimeoutThreshold) {
+                    LockSupport.parkNanos(this, nanosTimeout);
+                }
+                if (Thread.interrupted()) {
+                    throw new InterruptedException();
+                }
+            }
+        } finally {
+            if (failed) {
+                cancelAcquire(node);
+            }
+        }
+    }
+
+    private void doAcquireShared(int arg) {
+        final Node node = addWaiter(Node.SHARED);
+        boolean failed = true;
+        try {
+            boolean interrupted = false;
+            for (; ; ) {
+                final Node p = node.predecessor();
+                if (p == head) {
+                    int r = tryAcquireShared(arg);
+                    if (r >= 0) {
+                        setHeadAndPropagate(node, r);
+                        p.next = null;
+                        if (interrupted) {
+                            selfInterrupt();
+                        }
+                        failed = false;
+                        return;
+                    }
+                }
+                if (shouldParkAfterFailedAcquire(p, node) && parkAndCheckInterrupt()) {
+                    interrupted = true;
+                }
+            }
+        } finally {
+            if (failed) {
+                cancelAcquire(node);
+            }
+        }
+    }
+
+    private Node enq(final Node node) {
+        for (; ; ) {
+            Node t = tail;
+            if (t == null) {
+                if (compareAndSetHead(new Node())) {
+                    tail = head;
+                }
+            } else {
+                node.prev = t;
+                if (compareAndSetTail(t, node)) {
+                    t.next = node;
+                    return t;
+                }
+            }
+        }
+    }
+
+    private final boolean parkAndCheckInterrupt() {
+        LockSupport.park(this);
+        return Thread.interrupted();
+    }
+
+    public final boolean release(int arg) {
+        if (tryRelease(arg)) {
+            Node h = head;
+            if (h != null && h.waitStatus != 0) {
+                unparkSuccessor(h);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    static void selfInterrupt() {
+        Thread.currentThread().interrupt();
+    }
+
+    private void setHead(Node node) {
+        head = node;
+        node.thread = null;
+        node.prev = null;
+    }
+
+    private static boolean shouldParkAfterFailedAcquire(Node pred, Node node) {
+        int ws = pred.waitStatus;
+        if (ws == Node.SIGNAL) {
+            return true;
+        }
+        if (ws > 0) {
+            do {
+                node.prev = pred = pred.prev;
+            } while (pred.waitStatus > 0);
+            pred.next = node;
+        } else {
+            compareAndSetWaitStatus(pred, ws, Node.SIGNAL);
+        }
+        return false;
+    }
+
+    public final boolean tryAcquireNanos(int arg, long nanosTimeout) throws InterruptedException {
+        if (Thread.interrupted()) {
+            throw new InterruptedException();
+        }
+        return tryAcquire(arg) || doAcquireNanos(arg, nanosTimeout);
+    }
+
+    private void unparkSuccessor(Node node) {
+        int ws = node.waitStatus;
+        if (ws < 0) {
+            compareAndSetWaitStatus(node, ws, 0);
+        }
+        Node s = node.next;
+        if (s == null || s.waitStatus > 0) {
+            s = null;
+            for (Node t = tail; t != null && t != node; t = t.prev) {
+                if (t.waitStatus <= 0) {
+                    s = t;
+                }
+            }
+        }
+        if (s != null) {
+            LockSupport.unpark(s.thread);
+        }
     }
     //----------------------------unsupported method------------------------
     protected boolean tryAcquire(int arg) {
@@ -30,7 +342,7 @@ public class MyAbstractQueueAynchronizer1_8 extends AbstractOwnableSynchronizer 
         throw new UnsupportedOperationException();
     }
 
-    protected boolean tryAcquireShared(int arg) {
+    protected int tryAcquireShared(int arg) {
         throw new UnsupportedOperationException();
     }
 
@@ -48,6 +360,7 @@ public class MyAbstractQueueAynchronizer1_8 extends AbstractOwnableSynchronizer 
     private static final long tailOffset;
     private static final long waitStatusOffset;
     private static final long nextOffset;
+    static final long spinForTimeoutThreshold = 1000L;
 
     static {
         try {
