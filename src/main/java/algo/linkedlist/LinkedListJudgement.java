a582e9d8221f5e70131604919e5e6b59fb9f67ae
wuping <403704156@qq.com>
2018-04-12 18:47:28 +0800

    2018041201

---
+++ src/main/java/algo/linkedlist/LinkedListJudgement.java
@@ -0,0 +1,21 @@
+package algo.linkedlist;
+
+public class LinkedListJudgement<E> {
+    public static void main(String[] args) {
+
+    }
+
+    public static class Node<E> {
+        private int val;
+        private Node next;
+
+        public Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void insertNode() {
+
+    }
+}
